e6dc6a7af589aa83f963f147e2873d664e279028
jomourin <jomourinagustin11@gmail.com>
2025-08-03 10:52:32 +0800

    Add files via upload

---
+++ DisplayMenu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <direct.h>
+#include <filesystem>
+
+using namespace std; // para dili mag sigeg std::
+
+int main() {
+    cout << "[1] List Files\n";
+    cout << "[2] Create Directory\n";
+    cout << "[3] Change Directory\n";
+    cout << "[4] Exit\n";
+    cout << "Enter choice: ";
+}
